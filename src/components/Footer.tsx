3e67c18b90dbd4600e0526e9aa1fc55228c4de3d
jhaseloh <joelina.haseloh@smail.th-koeln.de>
2025-01-05 05:02:24 +0100

    impressum

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Footer.scss'
+import Impressum from "./Impressum";
 
 function Footer() {
+  const [showImpressum, setShowImpressum] = useState(false);
+
+  const toggleImpressum = () => {
+    setShowImpressum(!showImpressum);
+  };
   return (
     <footer>
       <div>
-        <a href="https://www.linkedin.com/in/joelina-haseloh-540698269/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+      <a href="https://www.linkedin.com/in/joelina-haseloh-540698269/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
       </div>
-      <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a> with 💜</p>
-      <p>Impressum</p>
+      
+      <p><a onClick={toggleImpressum}>{showImpressum ? 'Impressum schließen' : 'Impressum'}</a></p>
+      {showImpressum && (
+      <Impressum />
+      )}
     </footer>
   );
 }
--- src/components/Impressum.tsx
+++ src/components/Impressum.tsx
@@ -1,25 +1,22 @@
 import React, { useState } from 'react';
 
-
-const Impressum: React.FC = () => {
-    const [isVisible, setIsVisible] = useState(false);
-
-    const toggleVisibility = () => {
-        setIsVisible(!isVisible);
-    };
+const Impressum = () => { 
 
     return (
         <div>
-            <footer>
-                <button onClick={toggleVisibility}>Impressum</button>
-            </footer>
-            {isVisible && (
                 <div className="impressum-content">
                     <h2>Impressum</h2>
-                    <p>Hier steht das Impressum...</p>
-                    <button onClick={toggleVisibility}>Schließen</button>
+                    <h4>Angaben gemäß § 5 TMG:</h4>
+                    <p>Joelina M. Haseloh <br />
+                    Dreikönigenstr. 37 <br />
+                    50997 Köln</p>
+
+                    <h4>Kontakt: </h4>
+                    <p>E-Mail: joe.haseloh@gmail.com</p>
+                    <p>Verantwortlich für den Inhalt nach § 55 RStV ist Joelina M. Haseloh</p>
+                    
+                    <h5>Portfolio Design Template von <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a></h5>
                 </div>
-            )}
         </div>
     );
 };
