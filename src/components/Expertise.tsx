af08c9da1d6109b179b9cee828ee7aa6f0ac3bf5
jhaseloh <joelina.haseloh@smail.th-koeln.de>
2025-01-05 04:37:30 +0100

    new icons and new profile picture

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faDocker, faPython } from '@fortawesome/free-brands-svg-icons';
+import { faCode, faPalette, faBolt } from '@fortawesome/free-solid-svg-icons';
 import Chip from '@mui/material/Chip';
 import '../assets/styles/Expertise.scss';
 
@@ -52,7 +52,7 @@ function Expertise() {
             <h1>Expertise</h1>
             <div className="skills-grid">
                 <div className="skill">
-                    <FontAwesomeIcon icon={faReact} size="3x"/>
+                    <FontAwesomeIcon icon={faCode} size="3x"/>
                     <h3>Coding</h3>
                     <p>Während der Ausbildung und vor Allem während des Studiums habe ich einiges im Bereich Programmierung gelernt. Angefangen mit einem Textadventure bis hin zur Programmierung einer AI die Ziffern und Buchstaben erlenen kann. </p>
                     <div className="flex-chips">
@@ -64,7 +64,7 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faDocker} size="3x"/>
+                    <FontAwesomeIcon icon={faPalette} size="3x"/>
                     <h3>Design</h3>
                     <p>Zum Design gehört nicht nur die Hardware, sondern auch die Software, sowie die Zusammenarbeit der Verschiedenen Abteilungen und Expertisen.</p>
                     <div className="flex-chips">
@@ -76,7 +76,7 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faPython} size="3x"/>
+                    <FontAwesomeIcon icon={faBolt} size="3x"/>
                     <h3>Elektrotechnik</h3>
                     <p>Durch meine Ausbildung und Arbeit in der Industrie bin ich in der Lage auch mal unter die Haube von elektronischen Geräten zu schauen</p>
                     <div className="flex-chips">
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Footer.scss'
 
@@ -7,10 +6,10 @@ function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a href="https://www.linkedin.com/in/joelina-haseloh-540698269/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
       </div>
       <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a> with 💜</p>
+      <p>Impressum</p>
     </footer>
   );
 }
