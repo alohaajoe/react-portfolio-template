32d629b0286d2f1a6d4c1245d26857d8bbaf0009
jhaseloh <joelina.haseloh@smail.th-koeln.de>
2025-01-05 14:08:51 +0100

    changed the icons and links on top of the page for mobile devices

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Main.scss';
 import profilePicture from '../assets/images/Joe.jpg';
@@ -20,8 +19,7 @@ function Main() {
           <p>Moin und Willkommen auf meinem Portfolio</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://www.linkedin.com/in/joelina-haseloh-540698269/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
         </div>
       </div>
