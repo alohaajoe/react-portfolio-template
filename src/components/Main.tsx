751771ed72a0b27c2c613377a71408457399d86f
jhaseloh <joelina.haseloh@smail.th-koeln.de>
2025-11-25 14:19:49 +0100

    new profile pictire

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Main.scss';
-import profilePicture from '../assets/images/Joe3.jpg';
+import profilePicture from '../assets/images/Joe4.jpg';
 
 function Main() {
 
